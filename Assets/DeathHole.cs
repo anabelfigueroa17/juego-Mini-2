a5a2d5f786b30a604efd1d3a34e914b876a301df
anabelfigueroa17 <anabel.fig.afg@gmail.com>
2019-05-21 23:53:06 -0700

    versión final mini 2

---
+++ Assets/DeathHole.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // If ball touches Death holes collider
+    // then isDead variable of BallControlScript is set to true
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        BallControlScript.setIsDeadTrue();
+    }
+}
